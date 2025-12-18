228bd7d479643b1187c873e093ba32a89234b9db
Joseph-Homiee <Joseph@Homiee314.onmicrosoft.com>
2025-12-19 10:05:18 +1100

    Add URL.canParse polyfill to Metro config for Node.js compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,14 @@
+if (typeof URL !== 'undefined' && !URL.canParse) {
+  URL.canParse = function(url, base) {
+    try {
+      new URL(url, base);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+}
+
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
@@ -5,4 +16,3 @@ const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('ts', 'tsx');
 
 module.exports = config;
-
