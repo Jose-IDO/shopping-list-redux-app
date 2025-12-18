5386b72f6805b02657b6c029d3f03aea90cbbf34
Joseph-Homiee <Joseph@Homiee314.onmicrosoft.com>
2025-12-18 23:16:45 +1100

    Integrate Redux Provider in App.js

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+import ShoppingListScreen from './src/screens/ShoppingListScreen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+      <ShoppingListScreen />
       <StatusBar style="auto" />
-    </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
