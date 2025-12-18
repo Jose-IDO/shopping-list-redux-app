78101fdf31bc512ee375551c7ebffe02d42688f3
Joseph-Homiee <Joseph@Homiee314.onmicrosoft.com>
2025-12-19 09:45:35 +1100

    Add web-specific root styling to fix blank screen

--- App.tsx
+++ App.tsx
@@ -4,13 +4,24 @@ import { Provider } from 'react-redux';
 import { store } from './src/store/store';
 import ShoppingListScreen from './src/screens/ShoppingListScreen';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Platform } from 'react-native';
 
 export default function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <ShoppingListScreen />
-      <StatusBar style="auto" />
+      <View style={Platform.OS === 'web' ? webStyles.root : undefined}>
+        <ShoppingListScreen />
+        <StatusBar style="auto" />
+      </View>
     </Provider>
   );
 }
+
+const webStyles = StyleSheet.create({
+  root: {
+    width: '100%',
+    height: '100vh',
+    overflow: 'auto',
+  },
+});
 
